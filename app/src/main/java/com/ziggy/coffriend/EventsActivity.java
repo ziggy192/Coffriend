4fa1359b5645630714aa9de7baab98263af3b805
NghiaLQ <luuquangnghia97@gmail.com>
2018-06-21 22:50:52 +0700

    Done Event Screen ( without navigation bar)

--- app/src/main/java/com/ziggy/coffriend/EventsActivity.java
+++ app/src/main/java/com/ziggy/coffriend/EventsActivity.java
@@ -1,13 +1,19 @@
 package com.ziggy.coffriend;
 
+import android.support.design.bottomappbar.BottomAppBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class EventsActivity extends AppCompatActivity {
 
+    BottomAppBar bar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_events);
+        bar = findViewById(R.id.bar);
+
+        setSupportActionBar(bar);
     }
+
 }

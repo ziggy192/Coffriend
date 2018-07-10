b15c0e7a42cfc0c5b7f012866de55ff344d6e738
NghiaLQ <luuquangnghia97@gmail.com>
2018-07-10 18:00:17 +0700

    Add FollowingsFragment & EventBus Library

--- app/src/main/java/com/ziggy/coffriend/AppBaseActivity.java
+++ app/src/main/java/com/ziggy/coffriend/AppBaseActivity.java
@@ -5,12 +5,15 @@ import android.content.res.Configuration;
 import android.support.annotation.NonNull;
 import android.support.design.bottomappbar.BottomAppBar;
 import android.support.design.widget.BottomSheetBehavior;
+import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.util.EventLog;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -19,19 +22,26 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.FrameLayout;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import org.greenrobot.eventbus.EventBus;
+import org.greenrobot.eventbus.Subscribe;
 
 import java.util.Objects;
 
-public class AppBaseActivity extends AppCompatActivity implements MenuBottomSheetDialogFragment.Listener{
+public class AppBaseActivity extends AppCompatActivity implements MenuBottomSheetDialogFragment.Listener {
 
 
     private static final String TAG = AppBaseActivity.class.toString();
 
     private BottomAppBar appBar;
     private MenuBottomSheetDialogFragment bottomSheetDialogFragment;
-
+    private TextView tvMonthTitle;
     private FrameLayout view_stub; //This is the framelayout to keep your content view
+    private FloatingActionButton floatingActionButton;
+
+    private ScrollController mScrollController;
 
 
     @Override
@@ -39,8 +49,8 @@ public class AppBaseActivity extends AppCompatActivity implements MenuBottomShee
         super.onCreate(savedInstanceState);
         super.setContentView(R.layout.activity_app_base);// The base layout that contains your navigation drawer.
         view_stub = findViewById(R.id.view_stub);
-
-
+        tvMonthTitle = findViewById(R.id.tvMonthTitle);
+        floatingActionButton = findViewById(R.id.fab);
 
         appBar = findViewById(R.id.bar);
         bottomSheetDialogFragment = new MenuBottomSheetDialogFragment();
@@ -51,11 +61,10 @@ public class AppBaseActivity extends AppCompatActivity implements MenuBottomShee
 //                sheetBehavior.setState(BottomSheetBehavior.STATE_EXPANDED);
 
 //                bottomSheetDialogFragment.show
-                bottomSheetDialogFragment.show(getSupportFragmentManager(),bottomSheetDialogFragment.getTag());
+                bottomSheetDialogFragment.show(getSupportFragmentManager(), bottomSheetDialogFragment.getTag());
             }
         });
 
-        appBar.replaceMenu(R.menu.bottom_app_bar_menu);
         appBar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
             @Override
             public boolean onMenuItemClick(MenuItem menuItem) {
@@ -64,18 +73,53 @@ public class AppBaseActivity extends AppCompatActivity implements MenuBottomShee
                         Intent intent = new Intent(AppBaseActivity.this, ChatListActivity.class);
                         startActivity(intent);
                         break;
+                    case R.id.nav_today:
+                        //todo scroll to middle
+                        mScrollController.scrollToToday();
+                        break;
+
                 }
                 return false;
             }
         });
 
 
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                gotoCreateHost(view);
+            }
+        });
+        floatingActionButton.setImageResource(R.drawable.ic_plus);
+        setupAppbarUIOrigional();
+
+
     }
+
+    public void setupAppbarUIOrigional() {
+        tvMonthTitle.setVisibility(View.GONE);
+        appBar.setFabAlignmentMode(BottomAppBar.FAB_ALIGNMENT_MODE_CENTER);
+        appBar.replaceMenu(R.menu.bottom_app_bar_menu);
+
+
+    }
+
+    public void setupAppbarUIFollowings() {
+        tvMonthTitle.setVisibility(View.VISIBLE);
+        appBar.setFabAlignmentMode(BottomAppBar.FAB_ALIGNMENT_MODE_END);
+        appBar.replaceMenu(R.menu.bottom_app_bar_menu_followings);
+
+//        floatingActionButton.setImageResource(R.drawable.ic_baseline_today_24px);
+//        floatingActionButton.setOnClickListener(null);
+
+    }
+
 
     @Override
     protected void onPostCreate(Bundle savedInstanceState) {
         super.onPostCreate(savedInstanceState);
-        navigateEventsFragment();
+        navigateFragement(EventsFragment.newInstance(null, null), "EventsFragment");
+
 //        mDrawerToggle.syncState();
     }
 
@@ -83,38 +127,6 @@ public class AppBaseActivity extends AppCompatActivity implements MenuBottomShee
     public void onConfigurationChanged(Configuration newConfig) {
         super.onConfigurationChanged(newConfig);
 //        mDrawerToggle.onConfigurationChanged(newConfig);
-    }
-
-    /* Override all setContentView methods to put the content view to the FrameLayout view_stub
-     * so that, we can make other activity implementations looks like normal activity subclasses.
-     */
-    @Override
-    public void setContentView(int layoutResID) {
-        if (view_stub != null) {
-            LayoutInflater inflater = (LayoutInflater) getSystemService(LAYOUT_INFLATER_SERVICE);
-            ViewGroup.LayoutParams lp = new ViewGroup.LayoutParams(
-                    ViewGroup.LayoutParams.MATCH_PARENT,
-                    ViewGroup.LayoutParams.MATCH_PARENT);
-            View stubView = inflater.inflate(layoutResID, view_stub, false);
-            view_stub.addView(stubView, lp);
-        }
-    }
-
-    @Override
-    public void setContentView(View view) {
-        if (view_stub != null) {
-            ViewGroup.LayoutParams lp = new ViewGroup.LayoutParams(
-                    ViewGroup.LayoutParams.MATCH_PARENT,
-                    ViewGroup.LayoutParams.MATCH_PARENT);
-            view_stub.addView(view, lp);
-        }
-    }
-
-    @Override
-    public void setContentView(View view, ViewGroup.LayoutParams params) {
-        if (view_stub != null) {
-            view_stub.addView(view, params);
-        }
     }
 
     @Override
@@ -170,41 +182,45 @@ public class AppBaseActivity extends AppCompatActivity implements MenuBottomShee
 //        return false;
 //    }
 
-    public void gotoHostDetail(View view){
+    public void gotoHostDetail(View view) {
         Intent intent = new Intent(this, HostDetailActivity.class);
         startActivity(intent);
+
+
     }
 
-    public void navigateEventsFragment() {
 
+    public void navigateFragement(Fragment fragment, String tag) {
+        Fragment mFragment = getSupportFragmentManager().findFragmentByTag(tag);
+        if (mFragment == null) {
+            mFragment = fragment;
+        }
         getSupportFragmentManager().beginTransaction()
-                .replace(R.id.view_stub, EventsFragment.newInstance(null, null), "EventsFragment")
+                .replace(R.id.view_stub, mFragment, tag)
+                .setCustomAnimations(android.R.anim.fade_in, android.R.anim.fade_out)
                 .commit();
 
     }
 
-    public void navigateSettings() {
-        getSupportFragmentManager().beginTransaction()
-                .replace(R.id.view_stub, SettingsFragment.newInstance(), "SettingsFragment")
-                .commit();
-
-    }
-
-    public void navigateHistoryFragment() {
-        getSupportFragmentManager().beginTransaction()
-                .replace(R.id.view_stub, HistoryListFragment.newInstance(), "HistoryFragment")
-                .commit();
-
-    }
     @Override
     public void onBottomSheetMenuItemClicked(int position) {
         switch (position) {
             case 0:
+                setupAppbarUIOrigional();
 
-                navigateEventsFragment();
+                navigateFragement(EventsFragment.newInstance(null, null), "EventsFragment");
                 break;
             case 1:
                 //followings
+                setupAppbarUIFollowings();
+                Fragment fragment = getSupportFragmentManager().findFragmentByTag("FollowingsFragment");
+                if (fragment == null) {
+                    fragment = new FollowingsFragment();
+                }
+                if (fragment instanceof ScrollController) {
+                    mScrollController = (ScrollController) fragment;
+                }
+                navigateFragement(fragment, "FollowingsFragment");
                 break;
             case 2:
                 //history
@@ -212,18 +228,56 @@ public class AppBaseActivity extends AppCompatActivity implements MenuBottomShee
 //                    Intent intent = new Intent(this, HistoryListActivity.class);
 //                    startActivity(intent);
 //                }
-                navigateHistoryFragment();
+                setupAppbarUIOrigional();
+                navigateFragement(HistoryListFragment.newInstance(), "HistoryFragment");
                 break;
             case 3:
                 //settings
-                navigateSettings();
+                setupAppbarUIOrigional();
+                tvMonthTitle.setVisibility(View.GONE);
+                appBar.setFabAlignmentMode(BottomAppBar.FAB_ALIGNMENT_MODE_CENTER);
+
+                navigateFragement(SettingsFragment.newInstance(), "SettingsFragment");
                 break;
         }
         Log.d(TAG, "onBottomSheetMenuItemClicked: " + position);
     }
 
-    public void gotoCreateHost(View view){
-        Intent intent = new Intent(this,CreateHostActivity.class);
+
+    public void gotoCreateHost(View view) {
+        Intent intent = new Intent(this, CreateHostActivity.class);
         startActivity(intent);
+
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        EventBus.getDefault().register(this);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        EventBus.getDefault().unregister(this);
+    }
+
+    @Subscribe
+    public void onMonthChangedEvent(MonthChangedEvent event){
+        tvMonthTitle.setText(event.newMonth);
+    }
+
+
+    public interface ScrollController {
+        void scrollToToday();
+    }
+
+    public static class MonthChangedEvent{
+        public String newMonth;
+
+        public MonthChangedEvent(String newMonth) {
+            this.newMonth = newMonth;
+        }
     }
 }
---
+++ app/src/main/java/com/ziggy/coffriend/FollowingsFragment.java
@@ -0,0 +1,164 @@
+package com.ziggy.coffriend;
+
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import org.greenrobot.eventbus.EventBus;
+
+import java.io.Console;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+import static android.support.constraint.Constraints.TAG;
+import static com.ziggy.coffriend.FollowingsFragment.FollowingsAdapter.TODAY_POSITION_IN_LIST;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class FollowingsFragment extends Fragment implements AppBaseActivity.ScrollController {
+    public static final String TAG = FollowingsFragment.class.toString();
+
+    public FollowingsFragment() {
+        // Required empty public constructor
+    }
+
+    RecyclerView recyclerView;
+    private int currentMonth = 0;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_followings, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        recyclerView = view.findViewById(R.id.rvFollowings);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        FollowingsAdapter adapter = new FollowingsAdapter();
+        recyclerView.setAdapter(adapter);
+
+        recyclerView.scrollToPosition(TODAY_POSITION_IN_LIST);
+        recyclerView.setOnScrollListener(new RecyclerView.OnScrollListener() {
+            @Override
+            public void onScrollStateChanged(@NonNull RecyclerView recyclerView, int newState) {
+                super.onScrollStateChanged(recyclerView, newState);
+            }
+
+            @Override
+            public void onScrolled(@NonNull RecyclerView recyclerView, int dx, int dy) {
+                super.onScrolled(recyclerView, dx, dy);
+                int position = ((LinearLayoutManager) recyclerView.getLayoutManager())
+                        .findFirstCompletelyVisibleItemPosition();
+                if (position == -1) {
+                    return;
+                }
+                FollowingModel model = new FollowingModel(position);
+                Log.d(TAG, String.format("onScrolled: firstVisiblePosition=%d, currentMonth=%d,newMonth=%d"
+                        , position, currentMonth, model.calendar.get(Calendar.MONTH)));
+
+                if (model.calendar.get(Calendar.MONTH) != currentMonth) {
+                    currentMonth = model.calendar.get(Calendar.MONTH);
+                    SimpleDateFormat dateFormat = new SimpleDateFormat();
+                    dateFormat.applyPattern("MMMM");
+                    String month = dateFormat.format(model.calendar.getTime());
+                    EventBus.getDefault().post(new AppBaseActivity.MonthChangedEvent(month));
+                }
+
+            }
+        });
+
+    }
+
+    @Override
+    public void scrollToToday() {
+        recyclerView.smoothScrollToPosition(TODAY_POSITION_IN_LIST);
+    }
+
+    private static class ViewHolder extends RecyclerView.ViewHolder {
+
+        TextView tvDate;
+        TextView tvDay;
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            tvDate = itemView.findViewById(R.id.tvDate);
+            tvDay = itemView.findViewById(R.id.tvDay);
+
+        }
+
+        public void bind(FollowingModel model) {
+            SimpleDateFormat dateFormat = new SimpleDateFormat();
+            dateFormat.applyPattern("EEE");
+            String day = dateFormat.format(model.calendar.getTime());
+            tvDay.setText(day);
+            Log.d(TAG, "bind: Day = " + day);
+
+            dateFormat.applyPattern("dd");
+            String date = dateFormat.format(model.calendar.getTime());
+            tvDate.setText(date);
+            Log.d(TAG, "bind: Date = " + date);
+        }
+    }
+
+    public static class FollowingsAdapter extends RecyclerView.Adapter<ViewHolder> {
+
+        private static final int MAX_LIST_LENGTH = 100;
+        public static final int TODAY_POSITION_IN_LIST =   50;
+
+
+
+        @Override
+        public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            return new ViewHolder(
+                    LayoutInflater.from(parent.getContext())
+                            .inflate(R.layout.item_followings_list, parent, false));
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull ViewHolder viewHolder, int i) {
+
+            //generate Model by position, no list needed
+            FollowingModel model =new FollowingModel(i);
+            viewHolder.bind(model);
+        }
+
+
+        @Override
+        public int getItemCount() {
+            return MAX_LIST_LENGTH;
+        }
+
+    }
+
+    private static class FollowingModel {
+        public Calendar calendar;
+
+        public FollowingModel(Calendar calendar) {
+            this.calendar = calendar;
+        }
+
+        public FollowingModel(int position) {
+            int addAmount = position - TODAY_POSITION_IN_LIST;
+            calendar = Calendar.getInstance();
+            calendar.add(Calendar.DATE, addAmount);
+        }
+    }
+
+
+}
